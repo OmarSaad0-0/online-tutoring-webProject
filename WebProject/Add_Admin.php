ed09dc2befd1942b3af0eec21b380b6056112fda
testt-101 <106129498+testt-101@users.noreply.github.com>
2022-05-24 04:27:47 +0200

    Delete Add_Admin.php

--- WebProject/Add_Admin.php
+++
@@ -1,9 +0,0 @@
-<!--This is a change-->
-<form method="POST" action="ApplyAdd.php">
-First name : <input type="text" name="fName"><br>    
-Last name : <input type="text" name="lName"><br>
-password : <input type="text" name="password"><br>
-Email : <input type="text" name="Email"><br>
-<input type="submit">
-</form>
-<!--okkkkk-->
